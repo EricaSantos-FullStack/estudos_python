19ea284d98faff76ad4814b51d2463a9b640aaed
Erica S. Francisco <71906862+EricaSantos-FullStack@users.noreply.github.com>
2021-10-25 21:59:58 -0300

    Create Quadrado-com-While-e-Else.py

---
+++ Quadrado-com-While-e-Else.py
@@ -0,0 +1,10 @@
+#Quadrado-com-While-e-Else
+
+from math import pow
+i = 15
+while i <= 200:
+  print('{} ao quadrado é {}'.format(i, pow(i, 2)))
+  i += 1
+
+else:
+     print("Python faz o sua mente pensar de forma diferente")
