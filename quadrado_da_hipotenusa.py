771746fed63072ec3e04fc3960da3061e00ad335
Erica S. Francisco <71906862+EricaSantos-FullStack@users.noreply.github.com>
2021-11-01 22:36:08 -0300

    Create quadrado_da_hipotenusa.py

---
+++ quadrado_da_hipotenusa.py
@@ -0,0 +1,10 @@
+# O quadrado da hipotenusa é igual à soma dos quadrados dos catetos.
+#√(x) = x(½) sendo c = √a2 + b2
+
+a = 3.0
+b = 4.0
+c = (a ** 2 + b ** 2) ** 0.5
+
+print("c =", c)
+
+#output = 5.0
