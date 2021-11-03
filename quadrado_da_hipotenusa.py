03adfb5d25d5206b0579bbd1dd3294de34cb7c7a
Erica S. Francisco <71906862+EricaSantos-FullStack@users.noreply.github.com>
2021-11-02 21:38:57 -0300

    agora usando o input do cliente

--- quadrado_da_hipotenusa.py
+++ quadrado_da_hipotenusa.py
@@ -1,10 +1,19 @@
 # O quadrado da hipotenusa é igual à soma dos quadrados dos catetos.
 #√(x) = x(½) sendo c = √a2 + b2
 
-a = 3.0
-b = 4.0
-c = (a ** 2 + b ** 2) ** 0.5
-
-print("c =", c)
+#a = 3.0
+#b = 4.0
+#c = (a ** 2 + b ** 2) ** 0.5
+#print("c =", c)
 
 #output = 5.0
+
+#--------------------------------------------------------
+
+#Agora usando o input do cliente
+
+a = float(input("Escreva o valor da primeira variável: "))
+b = float(input("Escreva o valor da segunda variável: "))
+formula = (a**2 + b**2) ** .5
+print("Hypotenuse length is", formula)
+
